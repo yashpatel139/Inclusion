1e9ab5557e194796861c3b3be2db149c589067e7
Manu9084 <manusaini22092003@gmail.com>
2024-07-26 12:57:38 +0530

    by manu

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,6 +4,11 @@ const Hero = () =>{
     return(
         <div>
             <h1>Herosection</h1>
+            <h1 className="text-center font-bold text-2xl"> Kya Bhaiyo Kb shuru karna hai </h1>
+            <div className="w-full  flex justify-center mt-10">
+            <img src="/Inc.png" />
+            </div>
+           
         </div>
     )
 }
