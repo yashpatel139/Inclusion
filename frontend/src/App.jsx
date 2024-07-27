b1e547d7d22adfc7c222e54ba1806cd6a59f67e5
ashworth-007 <137112086+ashworth-007@users.noreply.github.com>
2024-07-27 14:57:44 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,16 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Contact from './components/Contact';
 import About from './components/About';
-
+import Login from './components/Login';
+import Signup from './components/Signup';
+import ForgotPassword from './components/ForgotPassword';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
 
   return (
     <>
+       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <div className='nav-router bg-indigo-300'>
         <BrowserRouter>
           <Header/>
@@ -17,6 +21,9 @@ function App() {
             <Route path="/" element={<Hero/>} />
             <Route path="/contact" element={<Contact/>} />
             <Route path="/about" element={<About/>}/>
+              <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
           </Routes>
         </BrowserRouter>
       </div>
