c5e1b260e9c2ec2a119fb1f1b057b59119cbd682
ashworth-007 <137112086+ashworth-007@users.noreply.github.com>
2024-07-29 23:51:21 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import Signup from './components/Signup';
 import ForgotPassword from './components/ForgotPassword';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Articles from './components/Articles';
-
+import Body from './components/Body';
 function App() {
   // Ensure you are checking the environment variable correctly
   const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
@@ -27,6 +27,7 @@ function App() {
             <Route path="/signup" element={<Signup />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path='/articles' element={<Articles/>}/>
+              <Route path='/body' element={<Body/>}/>
           </Routes>
         </BrowserRouter>
       </div>
