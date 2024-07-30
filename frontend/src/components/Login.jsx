477df51d5af6b6824b1c3cb45d46798c0f0d22f6
ashworth-007 <137112086+ashworth-007@users.noreply.github.com>
2024-07-30 12:55:51 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,65 +1,113 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
-import { GoogleLogin } from '@react-oauth/google';
+import logo from '../assets/image.png'; // Import the local image
+import { useAuth } from '../AuthContext'; // Import the useAuth hook
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+  const { login } = useAuth(); // Access the login method from the context
 
-  const handleLogin = (e) => {
+  // Load stored email and password if 'Remember Me' was checked
+  useEffect(() => {
+    const storedEmail = localStorage.getItem('email');
+    const storedPassword = localStorage.getItem('password');
+    const storedRememberMe = localStorage.getItem('rememberMe') === 'true';
+
+    if (storedRememberMe) {
+      setEmail(storedEmail || '');
+      setPassword(storedPassword || '');
+      setRememberMe(storedRememberMe);
+    }
+  }, []);
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-    // Handle login logic here
-  };
+    setLoading(true);
+    setError('');
 
-  const handleGoogleLogin = (response) => {
-    // Handle Google login response here
-    console.log(response);
+    try {
+      const response = await fetch('http://localhost:5000/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+        // Handle successful login
+        if (result.message === "User Exists") {
+          // Update authentication context
+          login({ token: result.token, email });
+
+          // Decide storage based on 'Remember Me' checkbox
+          if (rememberMe) {
+            localStorage.setItem('authToken', result.token); // Store in localStorage
+            localStorage.setItem('email', email);
+            localStorage.setItem('password', password);
+            localStorage.setItem('rememberMe', 'true');
+          } else {
+            sessionStorage.setItem('authToken', result.token); // Store in sessionStorage
+            localStorage.removeItem('email');
+            localStorage.removeItem('password');
+            localStorage.removeItem('rememberMe');
+          }
+
+          // Redirect to home page
+          navigate('/');
+        } else {
+          setError(result.message );
+        }
+      } else {
+        setError(result.message );
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+      setError('An error occurred. Please try again later.');
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <div className="flex h-screen items-center justify-center bg-gradient-to-r from-indigo-200 via-indigo-300 to-purple-300">
-      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-
-        {/* Google Login Button */}
-        <div className="mb-4">
-          <GoogleLogin
-            onSuccess={handleGoogleLogin}
-            onError={(error) => console.error('Google login error:', error)}
-            logo={<img src="/google.svg" alt="Google logo" className="h-6 w-6" />}
-            buttonText="Login with Google"
-            className="w-full max-w-xs flex items-center justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white hover:bg-gray-50 mx-auto"
-          />
+    <div className="relative flex h-screen items-center justify-center bg-cover bg-center" style={{ backgroundImage: 'url(/login1.jpg)' }}>
+      <div className="absolute inset-0 bg-black opacity-40"></div> {/* Darker Overlay */}
+      <div className="relative w-full max-w-md p-8 bg-white bg-opacity-20 rounded-lg shadow-lg">
+        {/* Logo Image */}
+        <div className="mb-4 flex justify-center">
+          <img src={logo} alt="Login logo" className="h-16 w-16" /> {/* Increased image size */}
         </div>
-
-        {/* OR Divider */}
-        <div className="flex items-center justify-center my-4">
-          <span className="text-gray-500">OR</span>
-        </div>
+        <h2 className="text-2xl font-bold mb-6 text-center text-black">Login</h2>
 
         <form onSubmit={handleLogin} className="space-y-4">
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+            <label htmlFor="email" className="block text-sm font-medium text-black">Email</label>
             <input
               id="email"
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 text-black"
               required
             />
           </div>
           <div className="relative">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+            <label htmlFor="password" className="block text-sm font-medium text-black">Password</label>
             <input
               id="password"
               type={showPassword ? 'text' : 'password'}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 pr-10"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 pr-10 text-black"
               required
             />
             <button
@@ -82,21 +130,23 @@ const Login = () => {
               onChange={(e) => setRememberMe(e.target.checked)}
               className="h-4 w-4 text-indigo-600 border-gray-300 rounded"
             />
-            <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">Remember me</label>
+            <label htmlFor="remember-me" className="ml-2 block text-sm text-black">Remember me</label>
           </div>
           <button
             type="submit"
-            className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-700"
+            className="w-full bg-purple-600 border border-purple-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-purple-700 hover:border-purple-700 transition duration-300 ease-in-out"
+            disabled={loading}
           >
-            Login
+            {loading ? 'Logging in...' : 'Login'}
           </button>
+          {error && <p className="text-red-600 text-center mt-4">{error}</p>}
         </form>
         <div className="mt-4 flex flex-col items-center">
-          <Link to="/forgot-password" className="text-indigo-600 hover:text-indigo-700">
+          <Link to="/forgot-password" className="text-purple-900 hover:text-purple-700">
             Forgot Password?
           </Link>
-          <p className="mt-2 text-sm text-gray-600">
-            Don't have an account? <Link to="/signup" className="text-indigo-600 hover:text-indigo-700">Sign up</Link>
+          <p className="mt-2 text-sm text-black">
+            Don't have an account? <Link to="/signup" className="text-purple-900 hover:text-purple-700">Sign up</Link>
           </p>
         </div>
       </div>
