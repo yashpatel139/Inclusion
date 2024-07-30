d590f1ccdec7ab949535c491d52a93fcaee0fb3e
Yash Patel <yyashhpatel011@gmail.com>
2024-07-30 17:47:16 +0530

    Header file changed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,25 +2,24 @@ import { Menu, X } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useAuth } from '../AuthContext';
-import logo from '../assets/image.png';
 
 const Header = () => {
-  const [mobiledrawerOpen, setmobileDrawerOpen] = useState(false);
+//   const [mobiledrawerOpen, setmobileDrawerOpen] = useState(false);
   const { user, logout } = useAuth();
 
-  const toggleNavbar = () => {
-    setmobileDrawerOpen(prevState => !prevState);
-  };
+//   const toggleNavbar = () => {
+//     setmobileDrawerOpen(prevState => !prevState);
+//   };
 
   return (
-    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80 bg-indigo-500">
-      <div className="container px-4 mx-auto relative text-sm">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center flex-shrink-0">
-            <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
-            <span className="text-xl tracking-tight">Inclusion : Empowering India</span>
+    <nav className="z-50 py-3 backdrop-blur-lg bg-gray-200">
+      <div className=" m-2 p-4 bg-indigo-200 rounded-lg">
+        <div className="flex justify-between items-center font-semibold text-lg">
+          <div className="flex items-center pl-8">
+            <img className="h-16 w-18 mr-2 rounded-full" src="https://th.bing.com/th/id/OIP.VBYvbPBeY-IRDIC4GD2aCgHaE4?w=260&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="Logo"/>
+            <span className="text-2xl tracking-tight">Empowering India</span>
           </div>
-          <ul className="hidden lg:flex space-x-12 justify-center">
+          <ul className="lg:flex space-x-8 justify-end pr-8">
             <li>
               <Link to="/" className="hover:bg-gradient-to-r hover:from-orange-500 hover:to-red-800 hover:text-transparent hover:bg-clip-text">Home</Link>
             </li>
@@ -30,55 +29,23 @@ const Header = () => {
             <li>
               <Link to="/contact" className="hover:bg-gradient-to-r hover:from-orange-500 hover:to-red-800 hover:text-transparent hover:bg-clip-text">Contact</Link>
             </li>
-          </ul>
+          
           <div className="hidden lg:flex justify-center space-x-12 items-center">
             {user ? (
               <>
                 <span className="text-white">Welcome, {user.name}</span>
-                <Link to='/profile' className="py-2 px-3 border rounded-md bg-slate-400">Profile</Link>
-                <button onClick={logout} className="bg-gradient-to-r from-indigo-400 to-indigo-800 py-2 px-3 rounded-md">Logout</button>
+                <Link to='/profile' className="hover:bg-gradient-to-r hover:from-orange-500 hover:to-red-800 hover:text-transparent hover:bg-clip-text">Profile</Link>
+                <button onClick={logout} className="hover:bg-gradient-to-r hover:from-orange-500 hover:to-red-800 hover:text-transparent hover:bg-clip-text">Logout</button>
               </>
             ) : (
               <>
-                <Link to='/articles' className="py-2 px-3 border rounded-md bg-slate-400">Articles</Link>
-                <Link to='/login' className="bg-gradient-to-r from-indigo-400 to-indigo-800 py-2 px-3 rounded-md">Login / Signup</Link>
+                <Link to='/articles' className="hover:bg-gradient-to-r hover:from-orange-500 hover:to-red-800 hover:text-transparent hover:bg-clip-text">Articles</Link>
+                <Link to='/login' className="hover:bg-gradient-to-r hover:from-orange-500 hover:to-red-800 hover:text-transparent hover:bg-clip-text">Login/Signup</Link>
               </>
             )}
           </div>
-          <div className="lg:hidden md:flex flex-col justify-end">
-            <button onClick={toggleNavbar}>
-              {mobiledrawerOpen ? <X /> : <Menu />}
-            </button>
-          </div>
-        </div>
-        {mobiledrawerOpen && (
-          <div className="fixed right-0 z-20 w-full p-12 flex flex-col justify-center items-center lg:hidden">
-            <ul>
-              <li className="py-4 hover:bg-gradient-to-r hover:from-orange-500 hover:to-red-800 hover:text-transparent hover:bg-clip-text">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="py-4 hover:bg-gradient-to-r hover:from-orange-500 hover:to-red-800 hover:text-transparent hover:bg-clip-text">
-                <Link to="/about">About</Link>
-              </li>
-              <li className="py-4 hover:bg-gradient-to-r hover:from-orange-500 hover:to-red-800 hover:text-transparent hover:bg-clip-text">
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-            <div className="flex space-x-6">
-              {user ? (
-                <>
-                  <Link to='/profile' className="py-2 px-3 border rounded-md bg-slate-400">Profile</Link>
-                  <button onClick={logout} className="bg-gradient-to-r from-indigo-400 to-indigo-800 py-2 px-3 rounded-md">Logout</button>
-                </>
-              ) : (
-                <>
-                  <Link to='/articles' className="py-2 px-3 border rounded-md bg-slate-400">Articles</Link>
-                  <Link to='/login' className="bg-gradient-to-r from-indigo-400 to-indigo-800 py-2 px-3 rounded-md">Login / Signup</Link>
-                </>
-              )}
-            </div>
-          </div>
-        )}
+          </ul>
+      </div>
       </div>
     </nav>
   );
